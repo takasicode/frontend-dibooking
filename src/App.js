605acc5f4e20de76a4411c0c4fe24abb0f45b040
takasicode <infofadhil29@gmail.com>
2023-06-11 08:30:25 +0700

    update: register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Homepage';
-import Register from './pages/Register';
+import Register from './pages/Register/register';
 import Login from './pages/Login';
 import HomePageAccount from './pages/HomepageAccount';
 import AboutUs from './pages/AboutUs';
